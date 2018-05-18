71930971bf1eef0142579b160e868459648dbeb5
Filip <fdabek1@umbc.edu>
2018-05-17 23:49:45 -0400

    Comparing model results.

--- results.py
+++ results.py
@@ -1,9 +1,12 @@
+import matplotlib.pyplot as plt
 import pandas as pd
 import os
 
+RESULTS_PATH = 'results/'
+
 # Get all of the result files for the notebooks
 results = []
-for root, dirs, files in os.walk('results/'):
+for root, dirs, files in os.walk(RESULTS_PATH):
     for file in files:
         if file.endswith('.csv'):  # Must be a CSV
             results.append(file)
@@ -15,4 +18,10 @@ for result in results:
 
     df = pd.read_csv('results/' + result)
 
-    print('blah')
+    df = df[df['model'] != 'SGD']
+    df = df[df['model'] != 'RNNMultiple']
+    df = df[df['model'] != 'RNNAll']
+
+    df.plot(x='model', y=['train', 'test'], kind='bar', title=result, figsize=(15, 10), legend=True, fontsize=12)
+    plt.ylabel('MAE Loss (Mean Average Error)')
+    plt.show()
